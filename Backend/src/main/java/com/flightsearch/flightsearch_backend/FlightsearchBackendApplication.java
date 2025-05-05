70ae178ac23213532702f663d2f1ed93dfed3189
SamBSalgado <samuel.becerra@encora.com>
2025-05-05 11:12:58 -0600

    initial commit

---
+++ Backend/src/main/java/com/flightsearch/flightsearch_backend/FlightsearchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flightsearch.flightsearch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsearchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsearchBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/flightsearch/flightsearch_backend/FlightsearchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightsearch.flightsearch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsearchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
